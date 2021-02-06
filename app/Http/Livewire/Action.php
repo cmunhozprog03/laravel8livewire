e7ab509f2dffa4a24a51e888008dbe84efb5770b
cmunhozprog03h <cmunhozprog03@hotmail.com>
2021-02-06 14:55:55 -0300

    Lesson 4

--- app/Http/Livewire/Action.php
+++ app/Http/Livewire/Action.php
@@ -6,6 +6,26 @@ use Livewire\Component;
 
 class Action extends Component
 {
+
+    public $sum;
+    public $message;
+    public $num1;
+    public $num2;
+
+    public function addTwoNumbers($num1, $num2)
+    {
+        $this->sum = $num1 + $num2;
+    }
+
+    public function DisplayMessage($msg)
+    {
+        $this->message = $msg;
+    }
+
+    public function getSum(){
+        $this->sum = $this->num1 + $this->num2;
+    }
+
     public function render()
     {
         return view('livewire.action');
--- resources/views/livewire/action.blade.php
+++ resources/views/livewire/action.blade.php
@@ -1,3 +1,17 @@
 <div>
-    {{-- Success is as dangerous as failure. --}}
+    <button type="button" wire:click='addTwoNumbers(32,55)' >Sum</button>
+    <br>
+
+    <textarea wire:keydown.enter='DisplayMessage($event.target.value)'></textarea>
+    <br>
+
+    <form wire:submit.prevent='getSum'>
+        Enter Num1: <input type="text" name="num1" wire:model='num1'/>
+        Enter Num2: <input type="text" name="num2" wire:model='num2'/>
+        <button type="submit">Submit</button>
+    </form>
+
+    <hr>
+    Total da soma: {{$sum}} <br>
+    Message: {{$message}}
 </div>
